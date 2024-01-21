81d624561857bf4905a3c9b3118388b09e4db0fd
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-01-21 18:23:00 +0900

    [수정] 비밀번호 암호화 (2024.1.21 김도연)

--- src/main/java/teamcom/comfirstpro/SecurityConfig.java
+++ src/main/java/teamcom/comfirstpro/SecurityConfig.java
@@ -4,18 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    //처음 뜨는 로그인 페이지를 지움
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests //처음 뜨는 시큐리티 로그인 화면 해제
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
                 .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder(); //비밀번호 암호화
     }
 }

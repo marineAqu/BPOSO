9942166f161a38cf5d31265007a1db8d7205e8a2
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-01-27 20:17:16 +0900

    [추가] 길이, 패턴 등 valid 추가 (2024.1.27 김도연)

--- src/main/java/teamcom/comfirstpro/DTO/MemberDTO.java
+++ src/main/java/teamcom/comfirstpro/DTO/MemberDTO.java
@@ -1,5 +1,8 @@
 package teamcom.comfirstpro.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import teamcom.comfirstpro.entity.MemberEntity;
 
@@ -10,9 +13,21 @@ import teamcom.comfirstpro.entity.MemberEntity;
 @ToString
 public class MemberDTO {
     private Long id;
-    private String username;
-    private String memName;
-    private String password;
+
+    @Size(min = 3, max = 10, message = "최소 3자 이상, 10자 이하 영문 입력")
+    @Pattern(regexp = "^[a-z0-9]*$", message = "알파벳 소문자(a~z), 숫자(0~9)만 작성")
+    @NotBlank
+    private String username; //아이디
+
+    @Size(min = 1, max = 10, message = "최소 1자 이상, 10자 이하 입력")
+    @NotBlank
+    private String nickname; //닉네임
+
+    @Size(min = 4, max = 10, message = "최소 4자 이상, 10자 이하 입력")
+    @NotBlank
+    private String password; //비밀번호
+
+    private String role;
 
     public static MemberDTO toMemberDTO(MemberEntity memberEntity){
         MemberDTO memberDTO = new MemberDTO();
@@ -20,7 +35,7 @@ public class MemberDTO {
         memberDTO.setId(memberEntity.getId());
         memberDTO.setUsername(memberEntity.getUsername());
         memberDTO.setPassword(memberEntity.getPassword());
-        memberDTO.setMemName(memberEntity.getMemName());
+        memberDTO.setNickname(memberEntity.getNickname());
         return memberDTO;
     }
 

5319d22a6be67a2e146a60ad36fd196079bf2e13
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-01-27 20:11:48 +0900

    [추가] 아이디, 닉네임 중복 체크 (2024.1.27 김도연)

--- src/main/java/teamcom/comfirstpro/repository/MemberRepository.java
+++ src/main/java/teamcom/comfirstpro/repository/MemberRepository.java
@@ -3,10 +3,10 @@ package teamcom.comfirstpro.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamcom.comfirstpro.entity.MemberEntity;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     //아이디로 회원 정보 조회 (select * from member_table where member_email=?)
-    Optional<MemberEntity> findByUsername(String memId);
+    MemberEntity findByUsername(String memId);
+    boolean existsByUsername(String username);
+    boolean existsByNickname(String nickname);
 
 }

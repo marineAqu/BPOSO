d9ed6dea6c623a1222d3536c56c1445b99de5282
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-01-27 20:26:02 +0900

    [수정] 로그인한 사용자 정보를 받아 model에 addAttribute (2024.1.27 김도연)

--- src/main/java/teamcom/comfirstpro/controller/HomeController.java
+++ src/main/java/teamcom/comfirstpro/controller/HomeController.java
@@ -1,24 +1,20 @@
 package teamcom.comfirstpro.controller;
 
-import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
-    @GetMapping("/")
-    public String index(HttpSession session){
-        if(session.getAttribute("loginId") == null) session.setAttribute("loginId", "n");
-        /*
-        if (authentication != null && authentication.isAuthenticated()) {
-            // 로그인한 경우
-            System.out.println("로그인됨: 시큐리티");
-        } else {
-            // 로그인하지 않은 경우
-            System.out.println("로그인 되지않음: 시큐리티");
-        }
-         */
 
+    @GetMapping("/")
+    public String index(Model model, @AuthenticationPrincipal UserDetails userDetails){
+        //로그인 정보
+        if(userDetails != null) model.addAttribute("loginId", userDetails.getUsername());
         return "main";
     }
 }

49db0afe4e398f3f05bb4cf034ee5c2719882061
gawon <gawonlee0712@gmail.com>
2024-02-14 14:06:09 +0900

    Revert "Revert "Merge branch 'main' of https://github.com/marineAqu/BPOSO""
    This reverts commit 566630f4ef1580e83dfa8547532b0447c55f75a1.

--- src/main/java/teamcom/comfirstpro/config/SecurityConfig.java
+++ src/main/java/teamcom/comfirstpro/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package teamcom.comfirstpro.config;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,29 +8,49 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http, HttpSession session) throws Exception {
         http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests //처음 뜨는 시큐리티 로그인 화면 해제
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
                 .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화
 
         http.formLogin(login -> login
-                        .loginPage("/login") //보안이 필요한 페이지에 접근 시 login이라는 로그인 페이지에 리다이렉트
-                        .usernameParameter("username")
-                        .passwordParameter("password")
-                        .loginProcessingUrl("/login_process") //로그인이 수행되는 url
-                        .defaultSuccessUrl("/") //성공 시 리다이렉트 될 기본주소
-                        .failureForwardUrl("/login")); //실패 시 리다이렉트 될 기본주소
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login_process")
+                .successHandler(successHandler())
+                .permitAll());
+
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .permitAll()
+                .logoutSuccessHandler(logoutSuccessHandler()));
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder(); //비밀번호 암호화
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        SimpleUrlLogoutSuccessHandler successHandler = new SimpleUrlLogoutSuccessHandler();
+        successHandler.setUseReferer(true);
+        return successHandler;
     }
 }

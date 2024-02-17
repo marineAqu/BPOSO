5d50877c46e44138963037fdb8fd2c38472a9d34
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-02-17 11:53:11 +0900

    [추가] saveWantsee 추가 (2024.2. 17 김도연)

--- src/main/java/teamcom/comfirstpro/service/WantseeService.java
+++ src/main/java/teamcom/comfirstpro/service/WantseeService.java
@@ -2,6 +2,9 @@ package teamcom.comfirstpro.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import teamcom.comfirstpro.entity.WantseeEntity;
+import teamcom.comfirstpro.repository.MovieinfoRepository;
 import teamcom.comfirstpro.repository.WantseeRepository;
 
 @Service
@@ -9,6 +12,7 @@ import teamcom.comfirstpro.repository.WantseeRepository;
 public class WantseeService {
 
     private final WantseeRepository wantseeRepository;
+    private final MovieinfoRepository movieinfoRepository;
 
     public int IsMovieWantsee(String userId, Long movNo){
 
@@ -16,5 +20,20 @@ public class WantseeService {
 
         if(wantseeRepository.existsByUserIdAndMovNo(userId, movNo) == true) return 1;
         else return 0;
+    }
+
+    @Transactional
+    public void saveWantsee(String loginId, int heartChk, String movName){
+
+        Long movieNo = movieinfoRepository.findNoByMovieNm(movName).getNo();
+
+        //이미 보고싶어요가 눌린 경우 보고싶어요 삭제
+        if(heartChk == 1) wantseeRepository.deleteByUserIdAndMovNo(loginId, movieNo);
+
+        //보고싶어요가 이미 눌려있지 않은 경우 보고싶어요 저장
+        else {
+            WantseeEntity wantseeEntity = WantseeEntity.toWantseeEntity(loginId, movieNo);
+            wantseeRepository.save(wantseeEntity);
+        }
     }
 }

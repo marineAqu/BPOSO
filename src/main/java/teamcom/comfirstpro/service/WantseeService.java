0b7622a0230364faf1c6da36745279624cc8f6c6
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-02-19 16:57:21 +0900

    [수정] 코드 단순화 (2024.2. 19 김도연)

--- src/main/java/teamcom/comfirstpro/service/WantseeService.java
+++ src/main/java/teamcom/comfirstpro/service/WantseeService.java
@@ -22,7 +22,7 @@ public class WantseeService {
 
         //이미 보고싶어요 표시를 했을 시 1, 아닐 시 0
 
-        if(wantseeRepository.existsByUserIdAndMovNo(userId, movNo) == true) return 1;
+        if(wantseeRepository.existsByUserIdAndMovNo(userId, movNo)) return 1;
         else return 0;
     }
 

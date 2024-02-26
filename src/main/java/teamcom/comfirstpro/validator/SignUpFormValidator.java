c436e18ed2360d577109fbdd3d92485b4bd102e5
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-02-26 19:40:51 +0900

    [수정] findByNickname 메소드 자료형 수정에 따른 코드 수정 (nullPointer 예외) (2024.2. 26 김도연)

--- src/main/java/teamcom/comfirstpro/validator/SignUpFormValidator.java
+++ src/main/java/teamcom/comfirstpro/validator/SignUpFormValidator.java
@@ -34,14 +34,20 @@ public class SignUpFormValidator implements Validator {
     //마이페이지에서 정보 수정 시 사용하는 validate, 중복 검사 시 본인은 제외
     public void modifiedValidate(Object object, Errors errors, Long id) {
         MemberDTO memberDTO = (MemberDTO) object;
-        if(memberRepository.findByNickname(memberDTO.getNickname()).getId() != id){
-            errors.rejectValue("nickname", "invalid.nickname",
-                    new Object[]{memberDTO.getNickname()}, "이미 사용중인 닉네임입니다.");
-        }
 
-        if(memberRepository.findByUsername(memberDTO.getUsername()).getId() != id){
-            errors.rejectValue("username", "invalid.username",
-                    new Object[]{memberDTO.getUsername()}, "이미 사용중인 아이디입니다.");
-        }
+        //findBy 결과가 비어있지 않고 결과가 본인 (=수정하지 않은 경우)이 아닌 경우에 errors 표시
+        memberRepository.findByNickname(memberDTO.getNickname()).ifPresent(memberEntity -> {
+            if (memberEntity.getId() != id) {
+                errors.rejectValue("nickname", "invalid.nickname",
+                        new Object[]{memberDTO.getNickname()}, "이미 사용중인 닉네임입니다.");
+            }
+        });
+
+        try {
+            if(memberRepository.findByUsername(memberDTO.getUsername()).getId() == id){
+                errors.rejectValue("username", "invalid.username",
+                        new Object[]{memberDTO.getUsername()}, "이미 사용중인 아이디입니다.");
+            }
+        }catch (NullPointerException e){}
     }
 }

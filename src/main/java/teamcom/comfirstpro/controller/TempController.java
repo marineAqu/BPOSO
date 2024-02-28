5b86aae6d788c57910f41313097dee659981f514
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-02-28 18:25:41 +0900

    [수정] 세션 갱신 오류, 정보 수정 뒤 페이지 망가짐 오류 해결을 위한 modifyMyInfo 메소드 수정 (2024.2. 28 김도연)

--- src/main/java/teamcom/comfirstpro/controller/TempController.java
+++ src/main/java/teamcom/comfirstpro/controller/TempController.java
@@ -4,6 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,6 +41,11 @@ public class TempController {
     private final WantseeService wantseeService;
     private final PrincipalDetailsService principalDetailsService;
     private final MovieinfoService movieinfoService;
+
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("search-result")
     public String searchResult(@RequestParam("searchName") String searchName,
@@ -91,11 +100,12 @@ public class TempController {
         //<홈> 탭
         PrincipalDetails principalDetails = (PrincipalDetails) userDetails;
         model.addAttribute("userName", principalDetails.getNickName());
+        model.addAttribute("ModLoginId", userDetails.getUsername());
+        model.addAttribute("ModUserName", principalDetails.getNickName());
 
 
         //<보고싶어요> 탭
         model.addAttribute("wantseeList", wantseeService.myWantseeList(userDetails.getUsername()));
-
         //<후기리스트> 탭
         model.addAttribute("reviewList", reviewService.SearchMyReview(userDetails.getUsername()));
 
@@ -104,34 +114,57 @@ public class TempController {
     }
 
     @PostMapping("modifyMyInfo")
-    public String modifyMyInfo(@Valid MemberDTO memberDTO, Errors errors, Model model) {
+    public String modifyMyInfo(@Valid MemberDTO memberDTO, Errors errors, Model model, @AuthenticationPrincipal UserDetails userDetails) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        //기본적으로 추가되어야 하는 model 내용들
+        model.addAttribute("userName", userDetails.getUsername());
+        //<보고싶어요> 탭
+        model.addAttribute("wantseeList", wantseeService.myWantseeList(userDetails.getUsername()));
+        //<후기리스트> 탭
+        model.addAttribute("reviewList", reviewService.SearchMyReview(userDetails.getUsername()));
+
+
+        //비밀번호를 틀린 경우
+        if(!memberService.passwordValid(memberDTO.getPassword(), userDetails.getPassword())) {
+
+            model.addAttribute("loginId", userDetails.getUsername());
+            model.addAttribute("userName", ((PrincipalDetails) principal).getNickName());
+            model.addAttribute("ModLoginId", memberDTO.getUsername());
+            model.addAttribute("ModUserName", memberDTO.getNickname());
+            model.addAttribute("isModified", "비밀번호가 일치하지 않습니다.");
+            model.addAttribute("modifiedFail", "Y");
+
+            return "mypage";
+        }
 
         //변경사항이 없을 경우
         if(memberDTO.getNickname().equals(((PrincipalDetails) principal).getNickName()) && memberDTO.getUsername().equals(((PrincipalDetails) principal).getUsername())){
+            model.addAttribute("loginId", userDetails.getUsername());
+            model.addAttribute("userName", ((PrincipalDetails) principal).getNickName());
+            model.addAttribute("ModLoginId", memberDTO.getUsername());
+            model.addAttribute("ModUserName", memberDTO.getNickname());
             model.addAttribute("isModified", "수정된 내용이 없습니다.");
             model.addAttribute("modifiedFail", "Y");
             return "mypage";
         }
 
-        //패스워드는 수정사항이 없으므로 그대로 값을 제공
-        //memberDTO.setPassword(((PrincipalDetails) principal).getPassword());
-        //System.out.println("memberdto에 비밀번호 저장이 안 됨: "+memberDTO);
-        //값 검증
-        signUpFormValidator.modifiedValidate(memberDTO, errors, ((PrincipalDetails) principal).getNo()); //중복을 포함한 유효성 검사
+        //중복을 포함한 유효성 검사
+        signUpFormValidator.modifiedValidate(memberDTO, errors, ((PrincipalDetails) principal).getNo());
 
-        //(비밀번호는 제외하고) 닉네임과 아이디에 대한 오류가 있는지만 검증
+        //닉네임과 아이디에 대한 오류가 있는지만 검증
         if (errors.hasFieldErrors("username") || errors.hasFieldErrors("nickname")) {
-            System.out.println("에러 확인용:"+errors);
             //유효성 통과 못한 필드와 메시지를 핸들링
             Map<String, String> validatorResult = memberService.validateHandling(errors);
             for (String key : validatorResult.keySet()) {
                 model.addAttribute(key, validatorResult.get(key));
             }
             //유효성을 통과하지 못한 경우
+            model.addAttribute("loginId", userDetails.getUsername());
+            model.addAttribute("userName", ((PrincipalDetails) principal).getNickName());
             model.addAttribute("isModified", "정상적으로 수정되지 않았습니다.");
-            model.addAttribute("loginId", memberDTO.getUsername());
-            model.addAttribute("userName", memberDTO.getNickname());
+            model.addAttribute("ModLoginId", memberDTO.getUsername());
+            model.addAttribute("ModUserName", memberDTO.getNickname());
             model.addAttribute("modifiedFail", "Y");
             return "mypage";
         }
@@ -140,7 +173,16 @@ public class TempController {
             memberService.modifiyMemInfo(memberDTO, ((PrincipalDetails) principal).getUsername());
 
             //유효성을 통과한 경우
+            model.addAttribute("loginId", memberDTO.getUsername());
+            model.addAttribute("userName", memberDTO.getNickname());
+            model.addAttribute("ModLoginId", memberDTO.getUsername());
+            model.addAttribute("ModUserName", memberDTO.getNickname());
             model.addAttribute("isModified", "정상적으로 수정되었습니다.");
+
+            //정보가 변경되었으므로 세션을 갱신
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(memberDTO.getUsername(), memberDTO.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
             return "mypage";
         }
     }

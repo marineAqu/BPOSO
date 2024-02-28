21d923515090e4557677b85b323d19aebbae8b12
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-02-28 18:19:58 +0900

    [추가] 세션 갱신을 위한 authenticationManagerBean 추가 (2024.2. 28 김도연)

--- src/main/java/teamcom/comfirstpro/config/SecurityConfig.java
+++ src/main/java/teamcom/comfirstpro/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package teamcom.comfirstpro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -54,5 +56,10 @@ public class SecurityConfig {
         SimpleUrlLogoutSuccessHandler successHandler = new SimpleUrlLogoutSuccessHandler();
         successHandler.setUseReferer(true);
         return successHandler;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }

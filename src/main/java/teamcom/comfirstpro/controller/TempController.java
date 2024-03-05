fb1440012f1ec4b10fab60de9d6f5af503168629
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-03-05 15:19:57 +0900

    [수정] 로그인 실패 후 로그인 성공 시 발생하는 오류 해결 (2024.3.5 김도연)

--- src/main/java/teamcom/comfirstpro/controller/TempController.java
+++ src/main/java/teamcom/comfirstpro/controller/TempController.java
@@ -67,8 +67,9 @@ public class TempController {
     public String login(HttpServletRequest request, HttpSession session, @AuthenticationPrincipal UserDetails userDetails) {
         if(userDetails != null) return "main"; //이미 로그인했을 경우 메인으로
 
+        //로그인에 실패해서 request.getHeader("Referer")가 로그인 페이지일 경우 세션의 lastpage를 갱신하지 않음 (로그인 성공 후에 로그인 페이지를 이전 페이지로 인식해서 다시 로그인페이지로 돌아가지 않도록)
+        if(!request.getHeader("Referer").equals("http://localhost:8080/login")) session.setAttribute("lastPage", request.getHeader("Referer")); //로그인 성공 시 이전 페이지로 이동
 
-        session.setAttribute("lastPage", request.getHeader("Referer")); //로그인 성공 시 이전 페이지로 이동
         return "login";
     }
 
